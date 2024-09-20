940845c0172ddf60b39bed19e3ae0c764a6a26c6
Mohith Bacha <mohithbacha1@gmail.com>
2024-09-20 22:10:45 +0530

    Time: 9 ms (15.86%), Space: 23.6 MB (10.6%) - LeetHub

---
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        stringstream ss(s);
+        string word;
+        string ans;
+        while(ss>>word){
+            ans=word+" "+ans;
+        }
+        return ans.substr(0,ans.size()-1);
+    }
+};
