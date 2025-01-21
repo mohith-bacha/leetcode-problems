c5cee57e6c43ad770e84f021feea547cf92f4051
Mohith Bacha <mohithbacha1@gmail.com>
2025-01-21 21:35:21 +0530

    Time: 0 ms (100%), Space: 8.4 MB (58.35%) - LeetHub

---
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxDepth(string s) {
+        int count=0,maxi=0;
+        for(int i=0;i<s.length();i++){
+            if(s[i]=='('){
+                count++;
+            }
+            else if(s[i]==')'){
+                maxi=max(maxi,count);
+                count--;
+            }
+        }
+        return maxi;
+    }
+};
