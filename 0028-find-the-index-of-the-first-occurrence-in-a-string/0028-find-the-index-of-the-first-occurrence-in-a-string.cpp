e1fcf43051e50e1b7fdb9ae5abc61d7b943a72e7
Mohith Bacha <mohithbacha1@gmail.com>
2025-04-02 22:02:03 +0530

    Update 0028-find-the-index-of-the-first-occurrence-in-a-string.cpp

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -6,7 +6,7 @@ public:
         if(n<m){
             return -1;
         }
-        for (int i =0; i<= haystack.length() - needle.length(); ++i) {
+        for (int i =0; i<=haystack.length() - needle.length(); ++i) {
             if (haystack.substr(i, needle.length()) == needle) {
                 return i;
             }
