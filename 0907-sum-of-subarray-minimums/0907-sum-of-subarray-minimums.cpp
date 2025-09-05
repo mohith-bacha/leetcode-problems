3e39c2640bf105f36145037b2fbce7306ca06046
Mohith Bacha <mohithbacha1@gmail.com>
2025-09-05 22:21:28 +0530

    Time: 71 ms (5.79%), Space: 61.6 MB (5.39%) - LeetHub

--- 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.cpp
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.cpp
@@ -1,16 +1,25 @@
 class Solution {
 public:
     int sumSubarrayMins(vector<int>& arr) {
-        int MOD=1e9+7;
-        int n=arr.size();
-        int sum=0;
-        for(int i=0;i<n;i++){
-            int mini=arr[i];
-            for(int j=i;j<n;j++){
-                mini=min(mini,arr[j]);
-                sum+=mini%MOD;
+        stack<vector<int>> stack;
+        vector<int> res(arr.size(), 0);
+
+        for (int i = 0; i < arr.size(); i++) {
+            while (!stack.empty() && stack.top()[0] >= arr[i]) {
+                stack.pop();
             }
+
+            int j = stack.empty() ? -1 : stack.top()[1];
+            res[i] = stack.empty() ? arr[i] * (i + 1) : res[j] + arr[i] * (i - j);
+            stack.push({arr[i], i});
         }
-        return sum;
+
+        const long MOD = static_cast<long>(1e9 + 7);
+        long ans = 0;
+
+        for (int i = 0; i < arr.size(); i++) {
+            ans = (ans + static_cast<long>(res[i])) % MOD;
+        }
+        return static_cast<int>(ans);        
     }
 };
