791e22f78de3adc92a08920a6905751ebbd59682
Mohith Bacha <mohithbacha1@gmail.com>
2026-01-07 09:29:43 +0530

    Fix loop syntax in valid sudoku check

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -6,7 +6,7 @@ public:
         bool boxes[9][9] = {false};
 
         for (int i = 0; i<9; i++) {
-            for (int j = 0; j < 9; j++) {
+            for (int j = 0; j <9; j++) {
                 if (board[i][j] != '.') {
                     int num = board[i][j] - '1';
                     int boxIndex = (i / 3) * 3 + (j / 3);
